1752836af7cf9dd6b0f1b0ca9036882405eea09e
Jay A <jautus@hotmail.com>
2022-09-12 21:13:17 -0700

    clear all files

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const Appointments = require('./Appointments');
-const Doctors = require('./Doctors');
-const History = require('./History');
-const Patients = require('./Patients');
-const Schedules = require('./Schedules');
-
-module.exports = { Appointments, Doctors, History, Patients, Schedules };
